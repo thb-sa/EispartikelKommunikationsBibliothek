1ff71fe11596c49f23c591726ce09e263916af54
Pascal Helbig <pascalhelbig@gmail.com>
2016-11-22 21:40:07 +0100

    ein bisschen JavaDocs

--- src/main/java/datenKlassen/Kommunikator.java
+++ src/main/java/datenKlassen/Kommunikator.java
@@ -4,14 +4,29 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.net.Socket;
 
+/**
+ * Diese Klasse erzeugt eine Verbindung mit einem Server
+ */
 public class Kommunikator {
     private String host;
     private final static int PORT = 7000;
 
+    /**
+     * Erstellen einer neuen Instance
+     *
+     * @param host Der Host des Servers
+     *
+     */
     public Kommunikator(String host) {
         this.host = host;
     }
 
+    /**
+     * Diese Methode erstellt einen neue Socket Verbindung, die nach neuen Stationen schaut.
+     *
+     * @param listener
+     * @throws IOException
+     */
     public void setNeueStationListener(NeueStationenListener listener) throws IOException {
         Socket socket;
         try {
